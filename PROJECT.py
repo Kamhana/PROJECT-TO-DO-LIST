339c1677fcbe22873e2a45029264f6ef700c9405
Kamhana <99072658+Kamhana@users.noreply.github.com>
2023-03-18 23:28:13 +0700

    Add files via upload

---
+++ PROJECT.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidgetItem
+from PyQt5.uic import loadUi
+import sys
+# from qtconsole.qt import QtCore
+
+class Main(QMainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
+        loadUi("main.ui", self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    app.exec_()
