d6194f445bc2b003006051b3a90b063f5629b7c2
Kamhana <99072658+Kamhana@users.noreply.github.com>
2023-03-19 06:15:24 +0700

    Update PROJECT.py

--- PROJECT.py
+++ PROJECT.py
@@ -7,7 +7,22 @@ class Main(QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
         loadUi("main.ui", self)
+        todos = ["Kerjakan Projek", "Kerjakan PR", "Cuci mobil", "Ambil jemuran"]
+        for todo in todos:
+            item = QListWidgetItem(todo)
+            item.setFlags(item.flags() | QtCore.Qt.IterIsUserCHeckable)
+            item.setCheckState(QtCore.Qt.Unchecked)
+            self.todo_listWidget.addItem(item)
+        self.ToggleAllButton.clicked.connect(self.toggle_all)
 
+    def toggle_all(self):
+        for i in range(self.todo_listWidget.count()):
+            item = self.todo_listWidget.item(i)
+            if item.checkState() == QtCore.Qt.Checked:
+                item.setCheckState(QtCore.Qt.Checked)
+            else:
+                item.setCheckState(QtCore.Qt.Checked)
+                
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
